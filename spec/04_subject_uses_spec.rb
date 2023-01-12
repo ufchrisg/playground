6a29d6cb3ecd5f516ced78431d2379f2b77ab5c9
Chris Gross <chris@atob.com>
2023-01-12 16:33:40 -0500

    add x86_64-linux and fix tests

--- spec/04_subject_uses_spec.rb
+++ spec/04_subject_uses_spec.rb
@@ -4,14 +4,21 @@ require_relative '../app/playground'
 RSpec.describe Playground do
   subject(:playground) { Playground.new }
 
-  it { is_expected.to eql(playground.closed?) }
+  # Good
+  it 'is initially closed' do
+    expect(playground).to be_closed
+  end
+
+  # Bad
+  it { is_expected.to be_closed }
 
   describe '#open!' do
     subject(:open_playground) { playground.open! }
 
-    expect(open_playground).to be_closed
+    it 'opens the playground' do
+      expect { open_playground }.to change(playground, :open?).to(true)
+    end
 
-    specify { expect(open_playground).to be_closed }
-    it { is_expected.to be_truthy }
+    specify { expect { open_playground }.to change { playground.closed? }.to(false) }
   end
 end
